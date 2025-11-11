6038d8b211300e927322a7b9e8ba625e2152bf1f
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2025-11-11 02:37:07 +0000

    add run_zisk example commands

---
+++ run_zisk.sh
@@ -0,0 +1,7 @@
+./examples/scripts/go2wasm.sh
+
+./docker/wasm2c-package.sh /home/kev/work/skunkworks-go-wasm/examples/build-wasm/go/empty.wasm build/c-packages/
+
+./platform/zkvm/scripts/c2zkvm.sh /home/kev/work/skunkworks-go-wasm/build/c-packages build/riscv
+
+ziskemu -e /home/kev/work/skunkworks-go-wasm/build/riscv
