418e7c28d814c8bf88ca89a62a1fc60cc267255b
Marcin Bugaj <marcin@lita.foundation>
2025-12-12 12:08:25 +0100

    `stateless` via WASM with `wasmer` (cranelift)

--- go_benchmark.sh
+++ go_benchmark.sh
@@ -14,12 +14,18 @@ echo "Transpiling WASM to CWASM with wasmtime..."
 
 ./docker/docker-shell.sh /root/.wasmtime/bin/wasmtime compile --target riscv64gc-unknown-linux-gnu  examples/build-wasm/go/stateless.wasm -o examples/build-wasm/go/stateless-by-wasmtime/src/stateless.cwasm
 
+echo "Transpiling WASM to WASMU with wasmer..."
+
+# TODO: Cranelift is used for now because LLVM support is buggy. Once LLVM is fixed in wasmer use `--llvm` flag instead of `cranelift`; https://github.com/wasmerio/wasmer/issues/5951#issuecomment-3632904384
+./docker/docker-shell.sh /root/.wasmer/bin/wasmer compile --cranelift --target riscv64gc-unknown-linux-gnu  examples/build-wasm/go/stateless.wasm -o examples/build-wasm/go/stateless-by-wasmer/src/stateless.wasmu
+
 echo "Compiling C to RISCV..."
 
 OPT_LEVEL="-O0" ./platform/riscv-qemu-user/scripts/c2riscv-qemu-user.sh build/c-packages/stateless/ build/bin/stateless.riscv.O0.elf
 OPT_LEVEL="-O3 -fno-reorder-blocks" ./platform/riscv-qemu-user/scripts/c2riscv-qemu-user.sh build/c-packages/stateless/ build/bin/stateless.riscv.O3.elf
 (cd examples/go/stateless; GOOS=linux GOARCH=riscv64 go build -o ./stateless)
 (cd examples/build-wasm/go/stateless-by-wasmtime/; RUSTFLAGS='-C target-feature=+crt-static -C link-arg=-static'   cargo build --release --target riscv64gc-unknown-linux-gnu)
+(cd examples/build-wasm/go/stateless-by-wasmer/; RUSTFLAGS='-C target-feature=+crt-static -C link-arg=-static'   cargo build --release --target riscv64gc-unknown-linux-gnu)
 
 echo "Executing with qemu-riscv64"
 
@@ -29,6 +35,7 @@ echo "" > go_benchmark_results.txt
 ./docker/docker-shell.sh qemu-riscv64 -plugin /libinsn.so build/bin/stateless.riscv.O3.elf >> go_benchmark_results.txt 2>&1
 ./docker/docker-shell.sh qemu-riscv64 -plugin /libinsn.so examples/go/stateless/stateless >> go_benchmark_results.txt 2>&1
 ./docker/docker-shell.sh qemu-riscv64 -plugin /libinsn.so examples/build-wasm/go/stateless-by-wasmtime/target/riscv64gc-unknown-linux-gnu/release/standalone >> go_benchmark_results.txt 2>&1
+./docker/docker-shell.sh qemu-riscv64 -plugin /libinsn.so examples/build-wasm/go/stateless-by-wasmer/target/riscv64gc-unknown-linux-gnu/release/standalone >> go_benchmark_results.txt 2>&1
 
 echo "Done"
 
