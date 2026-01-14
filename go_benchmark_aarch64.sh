a2c2073bdb3ebd9ef2925654310885a393a4f7fe
Marcin Bugaj <47246918+marcinbugaj@users.noreply.github.com>
2026-01-14 12:43:34 +0100

    WAMR for dynamically linked AARCH64 Linux MUSL (#11)

--- go_benchmark_aarch64.sh
+++ go_benchmark_aarch64.sh
@@ -29,6 +29,11 @@ echo "Transpiling WASM to WASMU with wasmer(llvm)..."
 (cd examples/build-wasm/go/stateless-by-wasmer/; RUSTFLAGS='-C target-feature=+crt-static -C link-arg=-static'   cargo build --release --target aarch64-unknown-linux-musl)
 ./docker/docker-shell.sh qemu-aarch64 -plugin /libinsn.so examples/build-wasm/go/stateless-by-wasmer/target/aarch64-unknown-linux-musl/release/standalone >> go_benchmark_results_aarch64.txt 2>&1
 
+echo "Transpiling WASM to WAMR AOT with wamrc..."
+
+./platform/riscv-wamr-qemu-aarch64/scripts/wasm2wamr-qemu-aarch64.sh examples/build-wasm/go/stateless.wasm build/bin/stateless.wamr-aarch64.elf
+./docker/docker-shell.sh qemu-aarch64 -plugin /libinsn.so build/bin/stateless.wamr-aarch64.elf >> go_benchmark_results_aarch64.txt 2>&1
+
 echo "Direct compilation..."
 
 (cd examples/go/stateless; GOOS=linux GOARCH=arm64 go build -o ./stateless)
